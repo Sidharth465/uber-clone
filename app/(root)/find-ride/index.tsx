4601b688ec2db830bbb6c4b4480a5e87b0366efb
satyam verma <betterWaySid>
2024-10-29 14:03:00 +0530

    added sample.env

--- app/(root)/find-ride/index.tsx
+++ app/(root)/find-ride/index.tsx
@@ -1,12 +1,20 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
+import { useLocationStore } from "store";
 
 const FindRide = () => {
+  const {
+    userAddress,
+    destinationAddress,
+    setDestinationLocation,
+    setUserLocation,
+  } = useLocationStore();
   return (
-    <View>
-      <Text>FindRide</Text>
+    <View className="flex h-full justify-center items-center">
+      <Text>you are here {userAddress}</Text>
+      <Text>Destination: {destinationAddress}</Text>
     </View>
-  )
-}
+  );
+};
 
-export default FindRide
+export default FindRide;
